6852e79981df666b64d9dac1f70fdd056f2c37dc
AliSoheili <alisoheili1995@gmail.com>
2026-05-14 22:52:18 +0200

    JS added

---
+++ mpa-script.js
@@ -0,0 +1,97 @@
+jQuery(document).ready(function ($) {
+    function createCustomFieldHTML(i, mi) {
+        let options = '<option value="">-- Select Key --</option>';
+        if (window.mpa_meta_keys) {
+            mpa_meta_keys.forEach(key => {
+                options += `<option value="${key}">${key}</option>`;
+            });
+        }
+        return `
+        <div class="mpa-meta-row">
+            <select name="mpa-posts[${i}][meta][${mi}][key]">${options}</select>
+            <input type="text" name="mpa-posts[${i}][meta][${mi}][custom_key]" placeholder="Or custom key">
+            <input type="text" name="mpa-posts[${i}][meta][${mi}][value]" placeholder="Value">
+        </div>`;
+    }
+
+    function createPostBlock(i) {
+        return `
+        <div class="mpa-post-block">
+            <h3>Post ${i + 1}</h3>
+            <p><label>Title: <input type="text" name="mpa-posts[${i}][title]" style="width:70%;"></label></p>
+            <p>
+                <label>Content:</label><br>
+                <textarea id="editor-${i}" name="mpa-posts[${i}][content]"></textarea>
+            </p>
+            <p>
+                <label>Featured Image: 
+                    <button class="button mpa-upload" data-index="${i}">Upload Image</button>
+                    <input type="hidden" name="mpa-posts[${i}][image]" id="mpa-image-${i}">
+                    <span class="mpa-image-preview" id="mpa-preview-${i}"></span>
+                </label>
+            </p>
+            <p>
+                <label>Custom Fields: 
+                    <button type="button" class="button mpa-add-meta" data-index="${i}">Add Custom Field</button>
+                    <div class="mpa-custom-fields" id="mpa-meta-${i}"></div>
+                </label>
+            </p>
+            <hr>
+        </div>`;
+    }
+
+    function initEditor(id) {
+        if (typeof wp.editor !== 'undefined') {
+            wp.editor.remove(id);
+            wp.editor.initialize(id, {
+                tinymce: { wpautop: true, plugins: 'lists,textcolor,wordpress' },
+                quicktags: true,
+                mediaButtons: true
+            });
+        }
+    }
+
+    $('#mpa-count').on('change', function () {
+        const count = parseInt($(this).val());
+        const container = $('#mpa-posts-container');
+        container.empty();
+
+        for (let i = 0; i < count; i++) {
+            container.append(createPostBlock(i));
+        }
+
+        setTimeout(() => {
+            for (let i = 0; i < count; i++) {
+                initEditor(`editor-${i}`);
+            }
+        }, 200); // Delay to allow DOM to render
+    }).trigger('change');
+
+    $(document).on('click', '.mpa-upload', function (e) {
+        e.preventDefault();
+        const index = $(this).data('index');
+        const imageInput = $(`#mpa-image-${index}`);
+        const preview = $(`#mpa-preview-${index}`);
+
+        const frame = wp.media({
+            title: 'Select or Upload Image',
+            button: { text: 'Use this image' },
+            multiple: false
+        });
+
+        frame.on('select', function () {
+            const attachment = frame.state().get('selection').first().toJSON();
+            imageInput.val(attachment.id);
+            preview.html(`<img src="${attachment.url}" style="max-width:100px;">`);
+        });
+
+        frame.open();
+    });
+
+    $(document).on('click', '.mpa-add-meta', function () {
+        const index = $(this).data('index');
+        const container = $(`#mpa-meta-${index}`);
+        const metaIndex = container.children().length;
+        container.append(createCustomFieldHTML(index, metaIndex));
+    });
+});
